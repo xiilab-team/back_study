df8da8e56c41fa96e63daf624fd8d9f35ed2ccea
hc.park <hc.park@xiilab.com>
2022-07-19 11:37:59 +0900

    enum클래스 추가

--- src/main/java/com/example/demo/ExceptionHandler.java
+++ src/main/java/com/example/demo/ExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -24,6 +25,17 @@ public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionDTO> handleArgumentValidException(IllegalArgumentException exception, WebRequest webRequest) {
+        return new ResponseEntity<>(
+                ExceptionDTO.builder()
+                        .code(400)
+                        .timeStamp(LocalDateTime.now())
+                        .message(exception.getMessage())
+                        .build(), HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<ExceptionDTO> handleAInvalidFormatException(InvalidFormatException exception, WebRequest webRequest) {
         return new ResponseEntity<>(
                 ExceptionDTO.builder()
                         .code(400)
--- src/main/java/com/example/demo/MemberDTO.java
+++ src/main/java/com/example/demo/MemberDTO.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberDTO {
 
     @Builder
@@ -15,7 +12,7 @@ public class MemberDTO {
     public static class MemberRequestDTO {
         @NotNull(message = "이름이 null일순 없습니다.")
         private String name;
-        private String job;
+        private Job job;
     }
 
     @Builder
@@ -24,7 +21,7 @@ public class MemberDTO {
     public static class MemberResponseDTO {
         private Long id;
         private String name;
-        private String job;
+        private Job job;
     }
 
 
